10dbc6e4b9bc87ebb7ae76a9248dbf375397d9c0
Leonardo Campana <leonardobcampana@gmail.com>
2024-05-09 17:02:20 -0300

    Commit Inicial

---
+++ Models/InfosPokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaPokemons.Models
+{
+    public class InfosPokemon
+    {
+        public string height {  get; set; }
+        public string weight { get; set; }
+    }
+
+    public class PokemonHabilidades
+    {
+        public List<HabilidadeInfo> abilities { get; set; }
+    }
+
+
+    public class HabilidadeInfo
+    { 
+        public Habilidade ability { get; set; }
+    }
+
+
+    public class Habilidade
+    {
+        public string name { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft;
+using Newtonsoft.Json;
+using ConsultaPokemons;
+using ConsultaPokemons.Models;
+
+namespace ConsultaPokemons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int limit = int.Parse(Console.ReadLine());
+            string pokemonName = Console.ReadLine();
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemonName}");
+            var request = new RestRequest("", Method.Get);
+
+            var response = client.Execute(request);
+
+            InfosPokemon infos = JsonConvert.DeserializeObject<InfosPokemon>(response.Content);
+
+            PokemonHabilidades habilidades = JsonConvert.DeserializeObject<PokemonHabilidades>(response.Content);
+            
+            
+            
+            
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"Altura: {infos.height}");
+                Console.WriteLine($"Peso: {infos.weight}");
+                Console.WriteLine("Habilidades:");
+                foreach (var habilidade in habilidades.abilities)
+                {
+                    Console.WriteLine(habilidade.ability.name);
+                }
+                Console.WriteLine("----------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            
+        }
+    }
+}
